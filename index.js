3eee006d3a9aa97a03b39fb3d1f690bc97f61a11
Jalen Peeler <jalenpeeler@yahoo.com>
2021-04-04 05:23:46 -0400

    Done looping

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let names = [];
+function writeCards(people, surprise) {
+    for (let i = 0; i < people.length; i++) {
+        names.push(`Thank you, ${people[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return names;
+}
+
+function countDown(number){
+    while (number > 0) {
+        console.log(number--)
+    }
+    console.log(number)
+}
